461c7c9827b3a9d056b9e6d0197b2dda50bec95f
Nathan Figueroa <figuernd@miamioh.edu>
2019-04-08 14:02:49 -0400

    Completed example

---
+++ src/components/Photo.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="ma-2 photo">
+    <v-img v-bind:src="photo_url" />
+    <v-card-title>
+      <div class="text-truncate">{{title}}</div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    props: {
+      photo_url: String,
+      title: String,
+    }
+  }
+</script>
+
+<style>
+.photo {
+  width: 300px;
+}
+</style>
